1c3ca528b27a9c6d2f0bac5e6695d723bf4d7bf9
acparkgh <acpark123@gmail.com>
2021-04-24 01:19:04 -0400

    finished modal project

--- starter/script.js
+++ starter/script.js
@@ -7,6 +7,26 @@ const btnOpenModal = document.querySelectorAll('.show-modal');
 
 console.log(btnOpenModal);
 
-for ( let i = 0; i < btnOpenModal.length; i++ ) {
-  console.log(btnOpenModal[i].textContent);
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 }
+
+// function closeModal() {
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener("click", openModal);
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === "Escape" && !modal.classList.contains('hidden')) {
+    closeModal();
+  };
+});
